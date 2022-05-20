695babeec5130d3654ffde20657644d3d1162fd3
Evance23 <evancebarracks01@gmail.com>
2022-05-20 10:15:48 +0300

    Add addition form

--- Documents/Store/src/app/billing/billing.component.ts
+++ Documents/Store/src/app/billing/billing.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-billing',
@@ -6,10 +7,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./billing.component.css']
 })
 export class BillingComponent implements OnInit {
-
-  constructor() { }
+billingForm=this.fb.group(
+  {
+    product:[''],
+    cost:[''],
+    quantity:[''],
+    vat:[''],
+    billingDetails: this.fb.array( []) 
+  }
+)
+  
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+  }
+  addBillingDetails(){
+    const {product, cost, quantity, vat} = this.billingForm.value;
+    (this.billingForm.get('billingDetails') as FormArray).push(this.fb.group({product, cost, quantity, vat}))
+    this.billingForm.patchValue({
+      product:'',
+      cost:0,
+      quantity:0,
+      vat:0,
+    }) 
+  }
+  get billingDetailsArray(){
+    return this.billingForm.get('billingDetails') as FormArray
   }
 
 }
